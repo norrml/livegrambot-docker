32cdea050b3dfdecc2b8832b685c96d801188a99
Nooboontoo <43723594+norrml@users.noreply.github.com>
2023-02-04 17:29:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-blocked = "Bot Error"#message that would be displayed if user has blocked you
-start = "Hello! Introduction! Send /help for More info."#start command
-ban = "Ban this fool (Used by Owner)"#ban message, used by admin
-unban = "Unbanned :) Be Nice."#unban message
-text_message = "Message Forwared! Wait 1 Second before sending."#answering_text to user
-banned = "Banned!"#if admin has blocked a user
-main_id = -12345 #id without quotes #can be group id
-TOKEN = "22222:33333333333333333"#bot token
-## /help should be set up in @botfather
+blocked = "bot not working, error"#message that would be displayed if user has blocked you
+start = "Hi! Send message to me, and I will FORWARD to Owner/Admin!"#start command
+ban = "Banned! " #ban message
+unban = "Unbanned."#unban message
+text_message = "Message Forwarded, PLease wait 1 Second before sending another Message."#answering_text to user
+banned = "you are blocked"  #if admin has blocked a user
+main_id = 2222222 #id without quotes #can be group id, get from google
+#main_id = -2222222
+TOKEN = "22222:222222222222222"#bot token
